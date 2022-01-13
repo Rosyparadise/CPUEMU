a542ce2827b50da1b7d4da6e6538b5dd78e52d9c
Eleni Navrozidou <72340658+elennavr@users.noreply.github.com>
2022-01-13 13:46:06 +0200

    Update FirstFit

--- assignment2/src/FirstFit.java
+++ assignment2/src/FirstFit.java
@@ -5,16 +5,24 @@ public class FirstFit extends MemoryAllocationAlgorithm {
     public FirstFit(int[] availableBlockSizes) {
         super(availableBlockSizes);
     }
-    
+
     public int fitProcess(Process p, ArrayList<MemorySlot> currentlyUsedMemorySlots) {
-        boolean fit = false;
-        int address = -1;
         /* TODO: you need to add some code here
          * Hint: this should return the memory address where the process was
          * loaded into if the process fits. In case the process doesn't fit, it
          * should return -1. */
-        
+        boolean fit = false;
+        int address = -1;
+        int pSize= p.getMemoryRequirements();
+        //updateProcessState
+        for (MemorySlot s:currentlyUsedMemorySlots){
+            if(s.getSizeAvail()-1>=pSize && !fit){
+                fit=true;
+                address=s.getStart();
+                s.setStart(s.getStart()+pSize-1);
+            }
+        }
         return address;
     }
-    
+
 }
