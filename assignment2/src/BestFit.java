9d25331cc3580f2b517ba4eb115ccbd1bd96b6e1
Eleni Navrozidou <72340658+elennavr@users.noreply.github.com>
2022-01-13 13:55:06 +0200

    Update BestFit

--- assignment2/src/BestFit.java
+++ assignment2/src/BestFit.java
@@ -5,16 +5,41 @@ public class BestFit extends MemoryAllocationAlgorithm {
     public BestFit(int[] availableBlockSizes) {
         super(availableBlockSizes);
     }
-    
+
     public int fitProcess(Process p, ArrayList<MemorySlot> currentlyUsedMemorySlots) {
         boolean fit = false;
         int address = -1;
+
+        int pSize = p.getMemoryRequirements();
+        int i = 0;
+        int blockNum = -1;
+        int closestToMemory = 2147483647 ;
+
+        while(i < currentlyUsedMemorySlots.size()) {
+            if (currentlyUsedMemorySlots.get(i).getSizeAvail() < pSize){
+                i++;
+                continue;
+            }else{
+                if(currentlyUsedMemorySlots.get(i).getSizeAvail() < closestToMemory){
+                    closestToMemory = currentlyUsedMemorySlots.get(i).getSizeAvail();
+                    address = currentlyUsedMemorySlots.get(i).getStart();
+                    fit = true;
+                    blockNum = i;
+                }
+            }
+            i++;
+        }
+
+        if(address > -1) {
+            currentlyUsedMemorySlots.get(blockNum).setStart(pSize + currentlyUsedMemorySlots.get(blockNum).getStart() - 1);
+        }
+
         /* TODO: you need to add some code here
          * Hint: this should return the memory address where the process was
          * loaded into if the process fits. In case the process doesn't fit, it
          * should return -1. */
-        
+
         return address;
     }
-    
+
 }
