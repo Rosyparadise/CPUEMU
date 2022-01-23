66ccdc8e022300f96eaf7d0e7b0aaecc55f313a7
Eleni Navrozidou <72340658+elennavr@users.noreply.github.com>
2022-01-23 22:47:25 +0200

    last version

--- assignment2/src/CPU.java
+++ assignment2/src/CPU.java
@@ -1,109 +1,182 @@
 import java.util.ArrayList;
 
-public class MMU {
+public class CPU {
+    
+    public static int clock = 0; // this should be incremented on every CPU cycle
 
-    private final int[] availableBlockSizes;
-    private MemoryAllocationAlgorithm algorithm;
-    private ArrayList<MemorySlot> currentlyUsedMemorySlots;
-    private ArrayList<Process> runningProcesses; //arraylist of the processes that are stored in the memory right now
+    private Scheduler scheduler;
+    private MMU mmu;
+    private Process[] processes;
+    //processes array is copied here. Easier to work with.
+    private ArrayList<Process> list_processes;
+    //USED FOR STATS
+    private ArrayList<Process> list_processes_STATS;
+    private int currentProcess;
+    //saves the currect object running on the cpu
+    private Process currentObjproc;
+    //flag that is used to decide when the program is terminated
+    private boolean enoughIsEnough;
+    //used to check if the most recently available process has a smaller burst time than the one currently running. (In case SRTF is used)
+    private boolean largerThan;
+    // used to represent if the quantum has reached its end. (In case RoundRobin is used)
+    private boolean RRchange;
+    //true in case a process has been terminated in the current cycle. (Is used to correctly identify an end state of the program)
+    private boolean removed;
 
-    public MMU(int[] availableBlockSizes, MemoryAllocationAlgorithm algorithm) {
-        this.availableBlockSizes = availableBlockSizes;
-        this.algorithm = algorithm;
-        this.currentlyUsedMemorySlots = new ArrayList<MemorySlot>();
-        this.runningProcesses= new ArrayList<Process>();
+    private int tempQuantum;
 
-        int i=1;
+    
+    public CPU(Scheduler scheduler, MMU mmu, Process[] processes) {
+        this.scheduler = scheduler;
+        this.mmu = mmu;
+        this.processes = processes;
+        list_processes = new ArrayList<>();
+        list_processes_STATS = new ArrayList<>();
+        currentProcess=-1;
+        currentObjproc=null;
+        enoughIsEnough=false;
+        largerThan=false;
+        RRchange=false;
+        tempQuantum=0;
 
-        //matching each memory slot with the size given in the availableBlockSizes
-        int previousEnd = availableBlockSizes[0] - 1;
-        currentlyUsedMemorySlots.add(new MemorySlot(0, previousEnd, 0, previousEnd));
 
-        while(i < availableBlockSizes.length){
-            currentlyUsedMemorySlots.add(new MemorySlot(previousEnd + 1, previousEnd + availableBlockSizes[i], previousEnd+1, previousEnd + availableBlockSizes[i])) ;
-            previousEnd += availableBlockSizes[i];
-            i++;
+        for (int i=0;i<processes.length;i++)
+        {
+            if (processes[i].getBurstTime()>0) {
+                list_processes.add(processes[i]);
+                list_processes_STATS.add(processes[i]);
+            }
         }
-    }
-
-    //sorts an arraylist of processes based on the addresses - ascending order
-    private void selectionSort(ArrayList<Process> array) {
-        for (int i = 0; i < array.size(); i++) {
-            int min = array.get(i).getAddress();
+        //all processes sorted in increasing order based on their arrival time
+        for (int i = 0; i < list_processes.size(); i++)
+        {
+            int min = list_processes.get(i).getArrivalTime();
             int minId = i;
-            for (int j = i+1; j < array.size(); j++) {
-                if (array.get(j).getAddress() < min) {
-                    min = array.get(j).getAddress();
+            for (int j = i+1; j < list_processes.size(); j++) {
+                if (list_processes.get(j).getArrivalTime() < min) {
+                    min = list_processes.get(j).getArrivalTime();
                     minId = j;
                 }
             }
-            Process temp = array.get(i);
-            array.set(i,array.get(minId));
-            array.set(minId,temp);
+            Process temp = list_processes.get(i);
+            list_processes.set(i,list_processes.get(minId));
+            list_processes.set(minId,temp);
         }
     }
 
-    //runs the given algorithm and returns whether or not the process fit in RAM
-    public boolean loadProcessIntoRAM(Process p) {
-        /* TODO: you need to add some code here
-         * Hint: this should return true if the process was able to fit into memory
-         * and false if not
-         * */
-        boolean fit = false;
-        updateMemory(); 
-        int add= algorithm.fitProcess(p, currentlyUsedMemorySlots); //address of the process is -1 if it did not fit and any other number if it did
-        if(add != -1){
-            fit = true;
-            p.getPCB().setState(ProcessState.READY,CPU.clock); 
-            p.setRAMarrivalClock(CPU.clock);
-            p.setAddress(add);
-            runningProcesses.add(p);
-        }
-        return fit;
-    }
-    
-    //checks for processes that may have been terminated and if so, organises the memory
-    private void updateMemory(){
-        //accessing the array of processes that have been added in the cpu
-        for (int i=0; i<runningProcesses.size(); i++){
-            //checking each process' state
-
-            if (runningProcesses.get(i).getPCB().getState()==ProcessState.TERMINATED){
-                //if terminated or ready it must be removed from cpu
-
-                //padress is the address given to the terminated process and also the start of the empty slot
-                int pAddress = runningProcesses.get(i).getAddress();
-                int pBlock=-1;
-                //pblock is the block where the terminated process is located
-                for (int k=0;k< currentlyUsedMemorySlots.size();k++) {
-                    if (pAddress>=currentlyUsedMemorySlots.get(k).getBlockStart() && pAddress<=currentlyUsedMemorySlots.get(k).getBlockEnd()) {
-
-                        pBlock=k;
-                        break;
+    //loads process into RAM when a process is terminated or an interruption has occurred.
+    private boolean nextProcess()
+    {
+        for (int i=0;i<list_processes.size();i++)
+        {
+            if (list_processes.get(i).getArrivalTime()<=clock)
+            {
+                if (currentProcess!=-1&&list_processes.get(i).getBurstTime()<currentObjproc.getBurstTime())
+                    largerThan=true;
+                if (largerThan ||currentProcess==-1)
+                {
+                    if(mmu.loadProcessIntoRAM(list_processes.get(i)))
+                    {
+                        System.out.println(clock+"  NEW->READY ");
+                        clock++;
+                        scheduler.addProcess(list_processes.get(i));
+                        list_processes.remove(list_processes.get(i));
+                        return true;
                     }
+                    else
+                        largerThan=false;
                 }
-                ArrayList<Process> processestobemoved = new ArrayList<>();
-
-                //accessing the following cpu running processes contained in the same block as the terminated process
-                for (int k=0;k<runningProcesses.size();k++) {
-                    if (runningProcesses.get(k).getAddress()>pAddress && runningProcesses.get(k).getAddress()<currentlyUsedMemorySlots.get(pBlock).getBlockEnd()) {
-                        processestobemoved.add(runningProcesses.get(k));
-                    }
-                }
-                //relocating all following addresses higher on the cpu table, the first process gets the address of the terminated one
-                if (processestobemoved.size()!=0) {
-                    selectionSort(processestobemoved);
-                    for (int k=0;k<processestobemoved.size();k++) {
-                        //the next free address that can accommodate a process
-                        processestobemoved.get(k).setAddress(pAddress);
-                        pAddress+=processestobemoved.get(k).getMemoryRequirements();
-
-                    }
-                }
-                currentlyUsedMemorySlots.get(pBlock).setStart(pAddress);
-                runningProcesses.remove(i);
-                i--;
             }
         }
+        return false;
+    }
+
+    //runs each clock cycle until enoughIsEnough is true
+    public void run() {
+        /* TODO: you need to add some code here
+         * Hint: you need to run tick() in a loop, until there is nothing else to do... */
+        while (!enoughIsEnough)
+            tick();
+
+        for (int i=0;i<list_processes.size();i++)
+        {
+            list_processes_STATS.remove(list_processes.get(i));
+        }
+
+
+        //Stat testing can be done here using list_processes_STATS
+    }
+    
+    public void tick() {
+        /* TODO: you need to add some code here
+         * Hint: this method should run once for every CPU cycle */
+
+        removed=false;
+        //gets next process if the process got terminated or quantum ended or new process has smaller bursttime.
+        if (currentProcess==-1 || (scheduler instanceof SRTF && !largerThan))
+        {
+            if (nextProcess()) {
+                //clock++;
+                return;
+            }
+        }
+
+        //scheduling algorithm is called when its time for a new process to start
+        if (currentProcess==-1 || largerThan || RRchange)
+        {
+            Process temp = scheduler.getNextProcess();
+            if (temp!=null)
+            {
+                currentObjproc = temp;
+                currentProcess=currentObjproc.getAddress();
+                System.out.println(clock+" READY->RUNNING");
+                System.out.println(clock+1+" READY->RUNNING");
+                clock+=2;
+                largerThan=false;
+                RRchange=false;
+                return;
+            }
+            System.out.println(clock +" NO OPERATION");
+        }
+
+        //process is in CPU
+        if (currentProcess!=-1)
+        {
+            //burst time is reduced by 1
+            currentObjproc.run();
+            System.out.print(clock+" BURST TIME REDUCED TO "+ currentObjproc.getBurstTime()+" ADDRESS IS "+ currentObjproc.getAddress());
+            //when the process is finished it gets terminated and its space freed up.
+            if (currentObjproc.getBurstTime() == 0) {
+                System.out.print(" RUNNING -> TERMINATED");
+                currentObjproc.getPCB().setState(ProcessState.TERMINATED, clock);
+                mmu.loadProcessIntoRAM(null);
+                scheduler.removeProcess(currentObjproc);
+                currentObjproc = null;
+                currentProcess = -1;
+                removed=true;
+            }
+            System.out.println();
+        }
+        //in case RR is used count how much of the quantum is left.
+        if (scheduler instanceof RoundRobin)
+        {
+            tempQuantum++;
+            if (tempQuantum== ((RoundRobin) scheduler).getQuantum())
+            {
+                RRchange=true;
+                tempQuantum=0;
+                currentObjproc = null;
+                currentProcess = -1;
+            }
+
+        }
+        /*program is terminated when there are no processes waiting to be brought to RAM, no processes in ram and no process in CPU.
+        * its also terminated when there is a process waiting to be situated in a memory slot but there can never be enough room for it.*/
+        if ( list_processes.size()==0 && currentProcess==-1 && scheduler.processes.size()==0 ||(currentProcess==-1 && scheduler.processes.size()==0 && clock+1>list_processes.get(list_processes.size()-1).getArrivalTime() && !removed))
+        {
+            System.out.println("PROCESSES LEFT IN NEW STATE: "+list_processes.size());
+            enoughIsEnough = true;
+        }
+        clock++;
     }
 }
